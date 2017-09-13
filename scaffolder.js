5930349ce7360a7ff4255a5f8d52e34134f713f6
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-09-13 21:28:45 +0200

    mods

--- scaffolder.js
+++ scaffolder.js
@@ -1,7 +1,12 @@
 'use strict';
 
-module.exports = {
-  variables: [
+const notNullValidator = (val) => val !== '';
 
+module.exports = {
+  inputs: [
+    { type: 'input', name: 'projectName', message: 'project name:', validate: val => val && !val.contains(' ') },
+    { type: 'input', name: 'description', message: 'description:', validate: notNullValidator },
+    { type: 'input', name: 'author', message: 'author:' },
+    { type: 'input', name: 'author', message: 'license:', default: 'MIT' }
   ]
 };
