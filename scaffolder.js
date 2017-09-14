e3ae4c57a2941d2e1b82b9e8563e3c4d39b15cab
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-09-14 16:16:12 +0200

    mods

--- scaffolder.js
+++ scaffolder.js
@@ -3,7 +3,6 @@
 module.exports = {
   source: 'src',
   inputs: [
-    { type: 'input', name: 'description', message: 'description:' },
     { type: 'input', name: 'author', message: 'author:' },
     { type: 'input', name: 'license', message: 'license:', default: 'MIT' }
   ]
