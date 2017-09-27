5b4b04411be41ccb4d48c6a29a7e7b9a6509a6b9
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-09-27 19:32:49 +0200

    fist stable version

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@
 
 const Valkyrie = require('aws-valkyrie');
 const app = new Valkyrie();
+const pkg = require('./package.json');
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+app.get('/info', (req, res) => res.json(pkg));
 
-app.all('*', (req, res) => {
-  res.sendStatus(404);
-});
+app.get('/home', (req, res) => res.send('hello world'));
+
+app.all('*', (req, res) => res.sendStatus(404));
 
 exports.handler = (...args) => app.listen(...args);
