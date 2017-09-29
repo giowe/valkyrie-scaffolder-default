561c75f85f42443bf7756cae41e1478fe4c41499
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-09-29 11:17:50 +0200

    added root info in scaffolder

--- scaffolder.js
+++ scaffolder.js
@@ -3,6 +3,7 @@
 module.exports = {
   source: 'src',
   handler: 'index.handler',
+  root: '/info',
   inputs: [
     { type: 'input', name: 'author', message: 'author:' },
     { type: 'input', name: 'license', message: 'license:', default: 'MIT' }
