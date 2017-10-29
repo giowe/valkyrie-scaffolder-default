e36e428a0d6823f40a1152b86a974ab2e48b5d4f
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-10-30 00:42:09 +0100

    upgraded valk and added body parser

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ const Valkyrie = require('aws-valkyrie');
 const app = new Valkyrie();
 const pkg = require('./package.json');
 
+//body parser middleware; it will become an external module;
+app.use((req, res, next) => {
+  try {
+    if (req.get('content-type').toLowerCase.includes('application/json')) {
+      req.body = JSON.parse(req.body);
+    }
+  }
+  catch (ignore) {}
+  next();
+});
+
+app.post('/payload-test', (req, res) => res.json(Object.assign({digestedBy: 'Valkyrie'}, req.body)));
+
 app.get('/info', (req, res) => res.json(pkg));
 
 app.get('/home', (req, res) => res.send('hello world'));
